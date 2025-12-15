42f24c17592aaef566df9860e52316804ec8b39a
Sophia Robinson (RIT Student) <smr2738@rit.edu>
2025-12-15 09:26:09 -0500

    Q1 fin.

--- shoppers.py
+++ shoppers.py
@@ -20,16 +20,45 @@ Write down the manual test by creating at least two items.
 class Item:
     __slots__ = ['__code', '__name', '__price']
     def __init__(self, code, name, price):
-        self.__code = ''
-        self.__name = ''
-        self.__price = 0
+        self.__code = code
+        self.__name = name
+        self.__price = price
 
+    def get_code(self, __code):
+        return self.__code
+    def get_name(self, __name):
+        return self.__name
+    def get_price(self, __price):
+        return self.__price
     
+    def set_code(self, code):
+        self.__code = code
+    def set_name(self, name):
+        self.__name = name
+    def set_price(self, price):
+        self.__price = price
+
+    def __str__(self):
+        return self.__name + " [" + self.__code + "] - $" + str(self.__price) 
+    
+    def __repr__(self):
+        return '(' + self.__code + ', ' \
+            + self.__name + ', ' \
+            + str(self.__price) + ')'
+        
 
 
 # manual test from main() method
 def main():
     i1 = Item('ABCD-1234', 'Blue Sweater', 22)
     i2 = Item('WXYZ-9876', 'Red Pants', 36)
+
+    print(repr(i1))
+    print(repr(i2))
+
+    if i1 == i2:
+        print('These are the same item')
+    else:
+        print('These items are different')
 
 if __name__ == "__main__":    main()
